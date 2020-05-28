cdfa78be962f9a0d9d18da6b5e9e691fd1aea81f
Eduardo Bittencourt <biteca@gmail.com>
2020-05-28 02:06:54 -0300

    adding JWT authentication

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '89e71e8988dada304f2946317dca46f8',
+    expiresIn: '1d',
+  },
+}
--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,19 +1,21 @@
-import AppointmentRepository from '../repositories/AppointmentRepositories'
-import CreateAppointmentService from '../services/CreateAppointmentService'
 import { getCustomRepository } from 'typeorm'
-
 import { Router } from 'express'
 import { parseISO } from 'date-fns'
 
+import AppointmentRepository from '../repositories/AppointmentRepositories'
+import CreateAppointmentService from '../services/CreateAppointmentService'
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated'
+
 const appointmentsRouter = Router()
 
-appointmentsRouter.get('/', async (req, res) => {
+appointmentsRouter.get('/', ensureAuthenticated, async (req, res) => {
   const appointmentsRepository = getCustomRepository(AppointmentRepository)
   const appointments = await appointmentsRepository.find()
   return res.json(appointments)
 })
 
-appointmentsRouter.post('/', async (req, res) => {
+appointmentsRouter.post('/', ensureAuthenticated, async (req, res) => {
   try {
     const { provider_id, date } = req.body
 
